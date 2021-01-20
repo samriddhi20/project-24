e2f09234468065207455cbf724e56091371a5455
samriddhi20 <75122649+samriddhi20@users.noreply.github.com>
2021-01-20 22:36:37 +0530

    Add files via upload

---
+++ dustbin.js
@@ -0,0 +1,19 @@
+class Dustbin{
+constructor(x,y,width,height){
+var options={
+isStatic:false,
+friction:1,
+density:5
+}
+this.body = Bodies.rectangle(x,y,width,height,options)
+this.width = width;
+this.height = height;
+World.add(world, this.body);
+}
+display(){
+ var pos =this.body.position;
+rectMode(CENTER);
+fill("yellow");
+rect(pos.x, pos.y, this.width, this.height);
+}
+}
